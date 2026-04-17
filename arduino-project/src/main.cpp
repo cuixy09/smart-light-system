da832225ef79971a2c6b5b4ef338baa41bdace3e
shujia-sja <shujia0513@gmail.com>
2026-04-18 00:04:14 +0800

    feat: 光敏电阻探测

--- arduino-project/src/main.cpp
+++ arduino-project/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #define LED_PIN 7
+#define PHOTO_PIN A0
 
 // 后续接入计算函数
 int computedBrightness(int);
@@ -7,12 +8,16 @@ int computedBrightness(int);
 void setup()
 {
   pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
+  // int photoValue = analogRead(PHOTO_PIN);
+  // Serial.println(photoValue);
+
   // 呼吸灯效果：使用正弦函数实现平滑亮度变化
   float brightness = (sin(millis() / 500.0) + 1) * 127.5;
   analogWrite(LED_PIN, (int)brightness);
-  delay(30);
+  delay(100);
 }
