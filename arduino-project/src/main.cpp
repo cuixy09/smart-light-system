a01dd5d0bc44178225b9cf6c4cae5a7fc21aa320
cuixy09 <591907060@qq.com>
2026-05-05 00:47:45 +0800

    fix:修复历史遗留问题

--- arduino-project/src/main.cpp
+++ arduino-project/src/main.cpp
@@ -1,74 +1,31 @@
 #include <Arduino.h>
-#define LED_PIN 7
-#define PHOTO_PIN A0
-#define IR_PIN A5
+#define LED_PIN 9;
+#define SS_PIN 2;
 
-int computedBrightness(int aqi)
-{
-  int sr = 360;
-  int ss = 1080;
-
-  int cur_min = (millis() / 1000) / 60;
-
-  bool is_night = (cur_min >= ss || cur_min < sr);
-  if (!is_night)
-    return 0;
-
-  int elapsed;
-  if (cur_min >= ss)
-    elapsed = cur_min - ss;
-  else
-    elapsed = (24 * 60 - ss) + cur_min;
-
-  int night_time = (24 * 60 - ss) + sr;
-  double lambda = elapsed * 1.0 / night_time;
-  double base = 255.0 * (1.0 - 2.0 * abs(lambda - 0.5));
-
-  double aqi_fac = 1.0 + (aqi - 50) / 200.0;
-  if (aqi_fac < 0.5)
-    aqi_fac = 0.5;
-  if (aqi_fac > 2.0)
-    aqi_fac = 2.0;
-
-  int pwm = (int)(base * aqi_fac);
-  if (pwm > 255)
-    pwm = 255;
-  if (pwm < 0)
-    pwm = 0;
-  return pwm;
-}
-
-void setup()
-{
+void setup() {
   pinMode(LED_PIN, OUTPUT);
-  pinMode(IR_PIN, INPUT);
+  pinMode(SS_PIN, INPUT);
   Serial.begin(9600);
+  Serial.setTimeout(50);
 }
 
-int aqi = 50;
+int num = 0;
+int prev = 0;
 
-void loop()
-{
-  int irValue = digitalRead(IR_PIN);
-  int brightness;
-
-  int photoValue = analogRead(PHOTO_PIN);
-  int photoBrightness = map(photoValue, 0, 1023, 255, 0);
-
-  if (irValue == LOW)
-  {
-    brightness = photoBrightness;
+void loop() {
+  if (Serial.available()) {
+    int tmp = Serial.parseInt();
+	if (tmp != 0 || lastValidNum == 0) {
+        num = tmp;
+        prev = num;
+    }
   }
-  else
-  {
-    brightness = 0;
+  int PWM = num;
+  int val = digitalRead(SS_PIN);
+  if (val == LOW) {
+    PWM += 60;
+    if (PWM > 255) PWM = 255;
   }
-
-  analogWrite(LED_PIN, brightness);
-  Serial.print("IR:");
-  Serial.print(irValue);
-  Serial.print(" brightness:");
-  Serial.println(brightness);
-  Serial.println(photoBrightness);
-  delay(1000);
+  analogWrite(PWM_PIN, PWM);
+  delay(50);
 }
